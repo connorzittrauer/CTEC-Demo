e9540712884734d5400689eb94c6888f128548c9
connor zittrauer <zittrauerconnor@gmail.com>
2026-03-25 19:39:54 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+
+func helloHandler(write http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(write, "HELLO DOG!")
+}
+
+func main() {
+	
+	// define route
+	http.HandleFunc("/", helloHandler)
+	
+	fmt.Println("Server running on http://localhost:8080")
+	
+	// defines port to be run on 
+	http.ListenAndServe(":8080", nil)
+
+}
